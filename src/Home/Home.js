7f97de65c83f6a86046af7b52536b96655764b87
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-03 17:29:10 +0000

    added text to homepage

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <>Home</>;
+  return (
+    <div className="text-center">
+      <h1>Location Journal</h1>
+    </div>
+  );
 };
 
 export default Home;
