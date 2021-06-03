a3c165d5af9f893b6c25e4e42641728359d5cf26
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-03 06:43:24 +0000

    imported ModalItem

--- src/Location/locationInfo.js
+++ src/Location/locationInfo.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "reactstrap";
 import { useParams } from "react-router-dom";
+import ModalItem from "../Modal/Modal";
+import Form from "./Form";
 
-const LocationInfo = () => {
+const LocationInfo = (props) => {
   let { id } = useParams();
+  const { className } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
 
   return (
-    <div className="page_header">
-      <p>{id}</p>
+    <>
+      <div className="page_header">
+        <p>{id}</p>
 
-      <div className="page_header_action">
-        <Button color="secondary">Edit</Button>{" "}
-        <Button color="danger">Remove</Button>
+        <div className="page_header_action">
+          <Button color="secondary">Edit</Button>{" "}
+          <Button color="danger">Remove</Button>
+        </div>
       </div>
-    </div>
+
+      <ModalItem toggle={toggle} currentState={modal} className={className}>
+        <Form />
+      </ModalItem>
+    </>
   );
 };
 
