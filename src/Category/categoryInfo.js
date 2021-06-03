2fa2a339f59bbca0caca67182c5d32e03d05ef7c
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-03 06:58:25 +0000

    added Edit fn to Category info

--- src/Category/categoryInfo.js
+++ src/Category/categoryInfo.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "reactstrap";
 import { useParams } from "react-router-dom";
 import { Location as locationData } from "../Location/model";
 import LocationItem from "../Location/locationItem";
+import ModalItem from "../Modal/Modal";
+import Form from "./Form";
 
-const CategoryInfo = () => {
+const CategoryInfo = (props) => {
   let { id } = useParams();
+
+  const { className } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
   return (
     <>
       <div className="page_header">
         <p>{id}</p>
 
         <div className="page_header_action">
-          <Button color="secondary">Edit</Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Edit
+          </Button>{" "}
           <Button color="danger">Remove</Button>
         </div>
       </div>
@@ -24,6 +34,10 @@ const CategoryInfo = () => {
             <LocationItem {...data} />
           </div>
         ))}
+
+      <ModalItem toggle={toggle} currentState={modal} className={className}>
+        <Form />
+      </ModalItem>
     </>
   );
 };
